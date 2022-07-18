9333c0097e167bc32bac7594e3bc97168d19e86d
jiaabid <jia.abid98@gmail.com>
2022-07-18 10:55:31 -0700

    converting html to pdf

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button
+      @click="download"
+      style="
+        height: 40px;
+        background-color: black;
+        color: white;
+        border-radius: 7px;
+      "
+    >
+      Download
+    </button>
+
+    <!-- add ref attribute to the container which has to be downloaded -->
+    <div ref="content">
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import { jsPDF } from "jspdf";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    download() {
+      // "l"= screen orientation 
+      // "px" = unit
+      // "a4" = pdf format
+      const pdf = new jsPDF("l","px","a4");
+      const html = this.$refs.content.innerHTML;
+      pdf.html(html, {
+        callback: (pdf) => {
+          pdf.save("Invoice.pdf");
+        },
+        x: 45,
+        y: 15,
+        width: 400, 
+        height:1240,
+        windowWidth: 800
+      });
+    },
+  },
+};
 </script>
 
 <style>
